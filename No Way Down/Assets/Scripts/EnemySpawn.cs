940649082530587d735bf49666d9d7c3a9b9d396
hzhang9 <hzhang9@uoregon.edu>
2022-04-29 08:31:44 -0700

    minor enemy spawn change

--- No Way Down/Assets/Scripts/EnemySpawn.cs
+++ No Way Down/Assets/Scripts/EnemySpawn.cs
@@ -7,14 +7,17 @@ public class EnemySpawn : MonoBehaviour
     public GameObject enemyRoot;
     GameObject enemy;
     float time = 0;
+    int maxEnemy = 5;
+    int currentEnemy = 0;
 
     void Update()
     {
-        if (time>5)
+        if (time>5 && currentEnemy < maxEnemy )
         {
             enemy = Instantiate(enemyRoot, transform.position, transform.rotation);
             enemy.transform.SetParent(transform);
             time = 0;
+            currentEnemy++;
         }
         time += Time.deltaTime;
     }
